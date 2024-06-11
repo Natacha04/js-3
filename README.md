55858ebb0d44f4f5b697b9fdaf83b51304c36d94
Natacha04 <163122214+Natacha04@users.noreply.github.com>
2024-06-11 08:20:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Natacha04/js-3)
---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Javascript est en place");
+
+let utilisateur = {
+  nom: ["Thais", "Dubois"],
+  age: 22,
+  ville: "Paris",
+  montant_panier: "100",
+  est_deja_client: "ici",
+  newsletter: Boolean
+}
+
+utilisateur.ville = "Marseille"
+
+console.log(utilisateur)
